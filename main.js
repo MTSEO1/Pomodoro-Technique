8591be7e9d30a6bdd45dd2ce1eca7dbbbbd186aa
Seo7829 <104804033+MTSEO1@users.noreply.github.com>
2022-08-20 16:18:40 +0900

    Function start, pause, reset

--- main.js
+++ main.js
@@ -2,30 +2,34 @@ const timerTxt = document.getElementById('timer');
 const startBtn = document.getElementById('start-btn');
 const cancelBtn = document.getElementById('cancel-btn');
 const pauseBtn = document.getElementById('pause-btn');
-// P Timer
+
+//Timer
 let time = '';
 let min = '';
 let sec = '';
-// Break Timer
-let breakTime = '';
-let breakMin = '';
-let breakSec = '';
 
-let state = 0; // 0 = standby, 1 = running, 2 = paused, 3 = resumed, 4 = break time
+let state = 0; // 0 = standby, 1 = running, 2 = paused, 3 = break time
+
+// let START_TIME = 0;
+// let BREAK_TIME = 3;
 
 const HIDDEN_CLASSNAME = 'hidden';
 
 let doit = null;
+
 const timer = () => {
   min = parseInt(time / 60);
   sec = time % 60;
   timerTxt.innerHTML = `${min} 분 ${sec} 초`;
   time--;
-  if (time < 0) {
+  if (time < 0 && state == 1) {
     clearTimeout(doit);
     timerTxt.innerHTML = '끝, 휴식 시작할까요?';
+    state = 3;
+  } else if (time < 0 && state == 3) {
+    clearTimeout(doit);
+    timerTxt.innerHTML = '휴식 끝, 다시 집중하시겠어요?';
     state = 0;
-    // breakTimer();
   }
 };
 
@@ -39,6 +43,9 @@ function onClickStart() {
   } else if (state == 2) {
     state = 1;
     time = remainedTime;
+    doit = setInterval(timer, 1000);
+  } else if (state == 3) {
+    time = 3;
     doit = setInterval(timer, 1000);
   }
 }
@@ -60,24 +67,3 @@ function onClickCancel() {
 startBtn.addEventListener('click', onClickStart);
 pauseBtn.addEventListener('click', onClickPause);
 cancelBtn.addEventListener('click', onClickCancel);
-
-// function breakTimer() {
-//   breakTime = 10;
-//   let bTimer = setInterval(function () {
-//     min = parseInt(breakTime / 60);
-//     sec = breakTime % 60;
-//     timerTxt.innerHTML = `${min} 분 ${sec} 초`;
-//     breakTime--;
-
-//     if (breakTime === 0) {
-//       clearTimeout(bTimer);
-//       timer.innerHTML = '휴식 끝';
-//       startBtn.classList.remove(HIDDEN_CLASSNAME);
-//       pauseBtn.classList.add(HIDDEN_CLASSNAME);
-//     }
-//   }, 1000);
-// }
-
-// function onClickCancel() {
-//   clearInterval(pTimer);
-// }
