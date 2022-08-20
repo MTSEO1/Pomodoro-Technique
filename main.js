f8bbe9f2364287ebc9b32a49dcc404ad148d7997
Seo7829 <104804033+MTSEO1@users.noreply.github.com>
2022-08-20 16:04:48 +0900

    Function reset

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let breakTime = '';
 let breakMin = '';
 let breakSec = '';
 
-let state = 0; // 0 = standby, 1 = running, 2 = paused, 3 = resumed
+let state = 0; // 0 = standby, 1 = running, 2 = paused, 3 = resumed, 4 = break time
 
 const HIDDEN_CLASSNAME = 'hidden';
 
@@ -24,6 +24,7 @@ const timer = () => {
   if (time < 0) {
     clearTimeout(doit);
     timerTxt.innerHTML = '끝, 휴식 시작할까요?';
+    state = 0;
     // breakTimer();
   }
 };
@@ -31,19 +32,34 @@ const timer = () => {
 function onClickStart() {
   startBtn.classList.add(HIDDEN_CLASSNAME);
   pauseBtn.classList.remove(HIDDEN_CLASSNAME);
-  time = 3;
-  doit = setInterval(timer, 1000);
+  if (state == 0) {
+    state = 1;
+    time = 10;
+    doit = setInterval(timer, 1000);
+  } else if (state == 2) {
+    state = 1;
+    time = remainedTime;
+    doit = setInterval(timer, 1000);
+  }
 }
 
 function onClickPause() {
   clearInterval(doit);
   remainedTime = time + 1;
+  state = 2;
   console.log('pause');
   console.log(remainedTime);
 }
+
+function onClickCancel() {
+  clearInterval(doit);
+  timerTxt.innerHTML = '종료';
+  state = 0;
+}
+
 startBtn.addEventListener('click', onClickStart);
 pauseBtn.addEventListener('click', onClickPause);
-// cancelBtn.addEventListener('click', onClickCancel);
+cancelBtn.addEventListener('click', onClickCancel);
 
 // function breakTimer() {
 //   breakTime = 10;
