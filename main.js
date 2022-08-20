a61a6bc2d33cd97ce989e7ae16afdda334f63129
Seo7829 <104804033+MTSEO1@users.noreply.github.com>
2022-08-20 15:52:15 +0900

    Function Pause

--- main.js
+++ main.js
@@ -1,54 +1,67 @@
-const timer = document.querySelector('.timer');
-const startBtn = document.querySelector('.start-btn');
-const cancelBtn = document.querySelector('.cancel-btn');
-const pauseBtn = document.querySelector('.pause-btn');
+const timerTxt = document.getElementById('timer');
+const startBtn = document.getElementById('start-btn');
+const cancelBtn = document.getElementById('cancel-btn');
+const pauseBtn = document.getElementById('pause-btn');
 // P Timer
-let time = 1500;
+let time = '';
 let min = '';
 let sec = '';
 // Break Timer
-let breakTime = 300;
+let breakTime = '';
 let breakMin = '';
 let breakSec = '';
 
-// function finishTimer() {
-//   console.log('Finishi! ');
-//}
+let state = 0; // 0 = standby, 1 = running, 2 = paused, 3 = resumed
+
+const HIDDEN_CLASSNAME = 'hidden';
+
+let doit = null;
+const timer = () => {
+  min = parseInt(time / 60);
+  sec = time % 60;
+  timerTxt.innerHTML = `${min} 분 ${sec} 초`;
+  time--;
+  if (time < 0) {
+    clearTimeout(doit);
+    timerTxt.innerHTML = '끝, 휴식 시작할까요?';
+    // breakTimer();
+  }
+};
 
 function onClickStart() {
-  let pTimer = setInterval(function () {
-    min = parseInt(time / 60);
-    sec = time % 60;
-    timer.innerHTML = `${min} 분 ${sec} 초`;
-    time--;
-
-    if (time < 0) {
-      clearTimeout(pTimer);
-      timer.innerHTML = '끝, 휴식 시작할까요?';
-      breakTimer();
-    }
-  }, 1000);
+  startBtn.classList.add(HIDDEN_CLASSNAME);
+  pauseBtn.classList.remove(HIDDEN_CLASSNAME);
+  time = 3;
+  doit = setInterval(timer, 1000);
 }
 
-function breakTimer() {
-  let bTimer = setInterval(function () {
-    min = parseInt(breakTime / 60);
-    sec = breakTime % 60;
-    timer.innerHTML = `${min} 분 ${sec} 초`;
-    breakTime--;
-
-    if (breakTime < 0) {
-      clearTimeout(bTimer);
-      timer.innerHTML = '휴식 끝';
-    }
-  }, 1000);
+function onClickPause() {
+  clearInterval(doit);
+  remainedTime = time + 1;
+  console.log('pause');
+  console.log(remainedTime);
 }
+startBtn.addEventListener('click', onClickStart);
+pauseBtn.addEventListener('click', onClickPause);
+// cancelBtn.addEventListener('click', onClickCancel);
+
+// function breakTimer() {
+//   breakTime = 10;
+//   let bTimer = setInterval(function () {
+//     min = parseInt(breakTime / 60);
+//     sec = breakTime % 60;
+//     timerTxt.innerHTML = `${min} 분 ${sec} 초`;
+//     breakTime--;
+
+//     if (breakTime === 0) {
+//       clearTimeout(bTimer);
+//       timer.innerHTML = '휴식 끝';
+//       startBtn.classList.remove(HIDDEN_CLASSNAME);
+//       pauseBtn.classList.add(HIDDEN_CLASSNAME);
+//     }
+//   }, 1000);
+// }
 
 // function onClickCancel() {
 //   clearInterval(pTimer);
 // }
-// function onClickPause() {}
-
-startBtn.addEventListener('click', onClickStart);
-// cancelBtn.addEventListener('click', onClickCancel);
-// pauseBtn.addEventListener('click', onClickPause);
